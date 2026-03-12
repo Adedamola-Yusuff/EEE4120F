257430efd3af9fe0d82e1562b8b6cfdf04e33d81
Adedamola Yusuff <adedamolayusuff341@gmail.com>
2026-03-12 03:42:47 +0200

    Added plotting  graphs for comparison and saving of the mandelbrot images

--- Practical-2/test.m
+++ Practical-2/test.m
@@ -1,6 +1,15 @@
-worker_counts = [2 3 4 5 6];   % Indeed, it is redundant to do the calculation 
+image_sizes = [
+    [800,600],   %SVGA
+    [1280,720],  %HD
+    [1920,1080], %Full HD
+    [2048,1080], %2K Cinema
+    [2560,1440], %2K QHD
+    [3840,2160], %4K UHD
+    [5120,2880], %5K
+    [7680,4320]  %8K UHD
+]
 
-for w = worker_counts
-    w
-    size(w)
-end
+names = ["SVGA","HD","FullHD","2K","QHD","4K UHD","5K","8K UHD"];
+workers = 2:6;
+
+fprintf("Measuring parallel execution time for %s with %d workers", names(3), workers(3))
